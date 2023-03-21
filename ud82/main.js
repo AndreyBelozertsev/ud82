ff25b64016545d9a1f3da39779b57c55ac1c5fb9
AndreyBelozertsev <andrey.belozertsev@gmail.com>
2023-03-21 23:48:57 +0300

    FirstCommit

---
+++ ud82/main.js
@@ -0,0 +1,3 @@
+import './src/js/app.js';
+import './style.css';
+import './src/css/main.css';
---
+++ ud82/tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      'xs': '375px',
+      'sm': '540px',
+      'md': '720px',
+      'lg': '960px',
+      'xl': '1140px',
+      '2xl': '1550px',
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '20px'
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
